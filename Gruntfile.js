422104e606e35d2cfb57cbfbc0705c59f88b2706
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-03 11:33:03 +0200

    Grunt: Add JSHint support.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@
  * http://opensource.org/licenses/MIT
  * ---------------------------------- */
 
+/* jshint node: true */
 module.exports = function(grunt) {
   'use strict';
 
@@ -122,6 +123,21 @@ module.exports = function(grunt) {
       docs: {}
     },
 
+    jshint: {
+      options: {
+        jshintrc: 'js/.jshintrc'
+      },
+      grunt: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: 'js/*.js'
+      },
+      docs: {
+        src: ['docs/assets/js/docs.js', 'docs/assets/js/fingerblast.js']
+      }
+    },
+
     validation: {
       options: {
         charset: 'utf-8',
@@ -160,8 +176,8 @@ module.exports = function(grunt) {
   grunt.registerTask('dist-js', ['concat', 'uglify']);
   grunt.registerTask('dist', ['dist-css', 'dist-js', 'copy']);
   grunt.registerTask('validate-html', ['jekyll', 'validation']);
-  grunt.registerTask('default', ['dist']);
   grunt.registerTask('build', ['dist']);
+  grunt.registerTask('default', ['dist']);
 
   // Version numbering task.
   // grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
